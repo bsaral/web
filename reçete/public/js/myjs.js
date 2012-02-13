1f53035d367168e5e0aae45cb15aa8cf460113fa
Betül Saral <betul.saral@bil.omu.edu.tr>
2012-02-13 03:01:35 +0200

    tarih problemli

--- reçete/public/js/myjs.js
+++
@@ -1,9 +0,0 @@
-function toggle(chkbox, txt) { 
-    var visSetting = (chkbox.checked) ? "visible" : "hidden"; 
-    document.getElementById(txt).style.visibility = visSetting; 
-} 
-
-function toggle_accord(chkbox) { 
-    var visSetting = (chkbox.checked) ? "visible" : "hidden"; 
-    document.getElementById("fs_content_accordion").style.visibility = visSetting; 
-} 
